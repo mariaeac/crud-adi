4a109300041fbbf02a2dc8d0914f850872829c86
Maria Eduarda <meac21kj@gmail.com>
2025-02-12 18:01:28 -0300

    initial commit

---
+++ src/main/java/com/meac/adi/AdiApplication.java
@@ -0,0 +1,13 @@
+package com.meac.adi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meac/adi/AdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meac.adi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
